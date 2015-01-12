cad7ada0fcf65e26f600e53e3fe1ec69582d81de
John Marsh <john.michael.marsh@gmail.com>
2015-01-12 17:38:26 -0500

    Initializing local git repo
    Signed-off-by: John Marsh <john.michael.marsh@gmail.com>

---
+++ HonoursProject/NoticeBoardViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HonoursProject
+//
+//  Created by John Marsh on 2014-11-20.
+//  Copyright (c) 2014 John Marsh. All rights reserved.
+//
+
+import UIKit
+
+class NoticeBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
